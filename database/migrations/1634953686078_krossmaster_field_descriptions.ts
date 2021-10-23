83947b8bd41fc67b542ec77e267db7d902a455f1
Filipe Braida <filipebraida@gmail.com>
2021-10-22 22:49:26 -0300

    change description size

---
+++ database/migrations/1634953686078_krossmaster_field_descriptions.ts
@@ -0,0 +1,16 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Krosmaster extends BaseSchema {
+  protected tableName = 'krosmaster'
+
+  public async up () {
+    this.schema.alterTable(this.tableName, (table) => {
+      table.string('description', 1024)
+    })
+  }
+
+  public async down () {
+    this.schema.alterTable(this.tableName, (table) => {
+    })
+  }
+}
